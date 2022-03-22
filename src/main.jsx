f2722489ef76cdfcff9d1eb3802c1218db1ee1ab
Lucas Kennedy <lukakfp@gmail.com>
2022-03-22 16:11:49 -0300

    chore: add biblioteca de componentes Chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import RoutesConfig from "./RoutesConfig";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <RoutesConfig />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <RoutesConfig />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );

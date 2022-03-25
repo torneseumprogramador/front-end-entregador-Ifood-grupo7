bb8c4b7ae0d9f425ff1c83d062ccba2084d0c504
Lucas Kennedy <lukakfp@gmail.com>
2022-03-25 12:15:48 -0300

    feat: adicionado headers com token

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+import getToken from "./getToken";
 
 const axiosConfig = axios.create({
   baseURL: "http://localhost:3001/",
+  headers: {
+    Authorization: getToken(),
+  },
 });
 
 export default axiosConfig;

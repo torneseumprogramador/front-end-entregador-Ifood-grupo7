43a0ccbc04acc799a2ac331d492275c6fef60fc8
Lucas Kennedy <lukakfp@gmail.com>
2022-03-30 17:32:46 -0300

    feat: axios config com base url provisória

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import getToken from "./getToken";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "http://localhost:8080/",
   headers: {
     Authorization: getToken(),
   },

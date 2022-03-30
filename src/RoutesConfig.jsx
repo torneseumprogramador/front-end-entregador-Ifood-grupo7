692c2f5aace4d3a82544db5a1733c595b95d87ae
Lucas Kennedy <lukakfp@gmail.com>
2022-03-30 17:38:15 -0300

    feat: adicionado um provider global para pulsação de tracking

--- src/RoutesConfig.jsx
+++ src/RoutesConfig.jsx
@@ -3,16 +3,19 @@ import Login from "./pages/Login";
 import Orders from "./pages/Orders";
 import OrderTraking from "./pages/OrderTraking";
 import PrivateRoute from "./pages/PrivateRoute";
+import { TrackingPulseProvider } from "./utils/contexts/TrackingPulse";
 
 function RoutesConfig() {
   return (
     <Routes>
-      <Route element={<PrivateRoute />}>
-        <Route path="/" element={<Orders />} />
-      </Route>
+      <Route element={<TrackingPulseProvider />}>
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Orders />} />
+        </Route>
 
-      <Route element={<PrivateRoute />}>
-        <Route path="/:orderid" element={<OrderTraking />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/:orderid" element={<OrderTraking />} />
+        </Route>
       </Route>
 
       <Route path="/login" element={<Login />} />
